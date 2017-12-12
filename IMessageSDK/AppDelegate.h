81c8732bcc474ab04a31a28e84df1baacc8f0ff7
jianghai <wiseuc_jianghai@163.com>
2017-12-12 17:03:07 +0800

    Initial Commit

---
+++ IMessageSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IMessageSDK
+//
+//  Created by JH on 2017/12/12.
+//  Copyright © 2017年 JiangHai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
