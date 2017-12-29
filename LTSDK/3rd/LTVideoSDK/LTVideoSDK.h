47869997edcf552d92bdef49e283cb922809b7c9
jianghai <wiseuc_jianghai@163.com>
2017-12-29 16:26:48 +0800

    LTSDK导入LTVideoSDK

---
+++ LTSDK/3rd/LTVideoSDK/LTVideoSDK.h
@@ -0,0 +1,24 @@
+//
+//  LTVideoSDK.h
+//  LTVideoSDK
+//
+//  Created by JH on 2017/12/29.
+//  Copyright © 2017年 JiangHai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#ifdef __OBJC__
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+#import <linphone/linphonecore.h>
+#import "LinphoneManager.h"
+#import "Log.h"
+#import "Utils.h"
+#endif
+
+
+@interface LTVideoSDK : NSObject
+
+@end
